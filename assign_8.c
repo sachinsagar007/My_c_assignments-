e9cd91c3fa4342eccad4ee0df3fe6106ba5816c6
sachinsagar007 <69853548+sachinsagar007@users.noreply.github.com>
2020-08-21 16:51:12 +0530

    Update assign_8.c

--- assign_8.c
+++ assign_8.c
@@ -3,13 +3,13 @@
 int main()
 {
     int *mem;
-    mem = (int *)malloc(3*1000*8);
+    mem = (int *)malloc(3*1000);
     if(mem != NULL)
         printf("Memory 3kB is allocated, address=%p\n", mem);
     else
         printf("Failed to allocate Memory 3kB\n");
 
-    mem = (int *)realloc(mem, 5*1000*8);
+    mem = (int *)realloc(mem, 5*1000);
     if(mem != NULL)
         printf("Memory 5kB is allocated, address=%p\n", mem);
     else
